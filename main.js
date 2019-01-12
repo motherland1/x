760adb03ea189de90955bb889bc1cc128f78146b
motherland1 <46547612+motherland1@users.noreply.github.com>
2019-01-12 01:40:55 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function setup() {
+    createCanvas(400, 400);
+    fill(255, 100, 120);
+    textSize(60);
+    textFont('Georgia');
+    text('Coming Soon.', 30, 60);
+}
+
+function draw() {
+
+}
